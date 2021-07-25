3717e3e7682c744af66bb84bc81c97543d4c8443
Eliana <ecano043@outlook.com>
2021-07-25 13:16:08 -0300

    IReceta e IUsuario

---
+++ src/cookey/dao/IUsuarioDAO.java
@@ -0,0 +1,15 @@
+package cookey.dao;
+
+import java.util.List;
+
+public interface IUsuarioDAO<EntidadDTO> {
+	public boolean createU(EntidadDTO e);
+
+	public boolean deleteU(Object clave, Object clave2);
+
+	public boolean updateU(EntidadDTO e);
+	
+	public EntidadDTO readU(String clave, String clave1);
+
+	public List<EntidadDTO> readAllU();
+}
